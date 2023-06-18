29f1abb8df5ea107f21b0e966fd06cc8c644ca96
Darshan-Paradox <darshan.paradox@gmail.com>
2023-06-18 18:02:27 +0530

    Handled python response and api response in backend/Controller/getUserInterest.js

--- api_testing/api_test.py
+++ api_testing/api_test.py
@@ -1,45 +1,38 @@
-import key
 import json
 import pprint
 import requests
 import sys
 
-# added
-keywords = sys.argv[1:-1]
-num = sys.argv[-1]
-#print(num)
-query = ','.join(keywords)
+keywords :list[str] = sys.argv[1:]
 
-springer_key :str = key.springer_key
+springer_key :str = "ba1b622422755eaaf763ec9b95b831d7" 
 
 base_url :str = "https://api.springernature.com/metadata"
 
 response_type :str = "json"
 search_index :int = 1
-num_response :int = num
-articles = []
-query_str :str = ''
 
-for keyword in keywords:
-    #print("hello inside")
-    query_str += keyword+'%20OR'
+num_response :int = 10
+articles :list[dict] = []
 
-# query_str :str = input("Enter the query string:\n")
-api_url = f"{base_url}/{response_type}?api_key={springer_key}&q={query_str}&s={search_index}&p={num_response}"
-print(api_url)
+for interest in keywords:
+    api_url = f"{base_url}/{response_type}?api_key={springer_key}&q={interest}&s={search_index}&p={num_response}"
 
-try:
+    try:
         
-    response = requests.get(api_url)
-    if response:
-        content = response.json()
-        pprint.pprint(content)
-        with open("test_response.json", "w+") as res:
-            res.write(json.dumps(content))
+        response = requests.get(api_url)
+        if response:
+            content = response.json()
+            articles += content["records"]
+        else:
+            print(response.status_code)
+
+    except Exception as err:
+        print(f"ERROR: {err}")
+
+while (True):
+    ind :int = int(input("Enter the record number 1-10:"))
+    if ind < 1:
+        break
     else:
-        print(response.status_code)
-except Exception as err:
-    print(f"ERROR: {err}")
-
-    #kksdindijks idjckdooefno rofsosojjjsps;sfusknfsdnlhalrhfn
-
+        pprint.pprint(articles[(ind - 1)%len(keywords)])
--- web-app/backend/Controllers/userInterest.js/getInterest.js
+++ web-app/backend/Controllers/userInterest.js/getInterest.js
@@ -22,36 +22,15 @@ const getInterest = async (req, res) => {
 
         // sending the array to python // added
         const keywords = user.interests
-<<<<<<< HEAD
-        const sendToPython = spawn('python3', ['/Users/aasthaprajapati/Documents/Coding/Hackathon/journal-reco/api_testing/api_test.py', ...keywords, 5])
-        sendToPython.stdout.on('data', (data) => {
 
-            // const responseString = data.toString().replace(/^\(|\)$/g, "");
-            // const response = JSON.parse(responseString)
-
-            console.log(`Python Output ${data}`);
+        const installDependencies = spawn("./bin/pip", ["install", "requests"]);
+        installDependencies.stdout.on("end", () => {
+            const sendToPython = spawn('./bin/python', ['/api.py', ...keywords])
+            sendToPython.stdout.on('end', () => {
+                console.log("Request sent to recommender")
+                //TODO: check if database has been updated or not
+            })
         })
-        sendToPython.stderr.on('data', (data) => {
-            console.error(`python error: ${data}`)
-        })
-=======
-        const pipInstall = spawn('./bin/pip', ["install"], "requests");
-
-        pipInstall.stdout.on("end", () =>
-            {
-                const sendToPython = spawn('./bin/python', ['/Users/aasthaprajapati/Documents/Coding/Hackathon/journal-reco/api_testing', ...keywords])
-
-                sendToPython.stdout.on('end', (data) => {
-                    //TODO: check the database if the field is updated or not
-                })
-
-                sendToPython.stderr.on('data', (data) => {
-                    console.error(`python error: ${data}`)
-                })
-            }
-        )
->>>>>>> d87f387 (Cleaned api_test file and created virtual env in controller for node to access python command and execute the api_test.py script)
-
     } catch (error) {
         console.log('Could not get interests');
         res.status(400)

c394cad1b54330efe0ef3d6651f3f3c8c3ec7dac
Darshan-Paradox <72197273+Darshan-Paradox@users.noreply.github.com>
2023-06-29 09:48:57 +0530

    Update getInterest.js

--- web-app/backend/Controllers/userInterest.js/getInterest.js
+++ web-app/backend/Controllers/userInterest.js/getInterest.js
@@ -23,18 +23,19 @@ const getInterest = async (req, res) => {
             key = key.replace(" ", "_")
         })
 
-        const installDependencies = spawn("pip3", ["install", "requests", "pymongo", "scikit-learn", "nltk", "pandas"]); // installing the required dependencies for the python script to run
+        //const installDependencies = spawn("pip3", ["install", "requests", "pymongo", "scikit-learn", "nltk", "pandas"]); // installing the required dependencies for the python script to run
         
-        installDependencies.stdout.on("end", () => {
+        //installDependencies.stdout.on("end", () => {
+
+        let py = "python3
+        if (process.platform === "win32" || process.platform == "win64") py = "python";
                 
-                //executing python script by spawning the child process
-                const sendToPython = spawn('python3', [`${process.cwd()}/backend/Controllers/userInterest.js/api_test.py`, ...keywords, uid])
-                sendToPython.stderr.on('data', (data)=>console.log(data.toString()))
-                sendToPython.stdout.on('data', (data) => {})
-                sendToPython.stdout.on('end', () => {
-                    console.log("Request sent to recommender")
-                })
-        })
+        //executing python script by spawning the child process
+        const sendToPython = spawn(py, [`${process.cwd()}/backend/Controllers/userInterest.js/api_test.py`, ...keywords, uid])
+        sendToPython.stderr.on('data', (data)=>console.log(data.toString()))
+        endToPython.stdout.on('data', (data) => {})
+        sendToPython.stdout.on('end', () => { console.log("Request sent to recommender") })
+        //})
     } catch (error) {
         console.log('Could not get interests');
         res.status(400)
